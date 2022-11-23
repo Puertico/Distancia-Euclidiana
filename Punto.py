f44d841a4c0260d8a9d8ec3fec2ce5764cf7885c
Cristian Puerto <capuertoc@correo.udistrital.edu.co>
2022-11-23 16:53:51 -0500

    v 0.00

---
+++ Punto.py
@@ -0,0 +1,16 @@
+import math
+
+
+class Punto:
+
+def __init__(self):
+
+    self.x=int
+    self.y=int
+
+def Punto(self):
+    self.punto=[self.x,self.y]
+
+def Distancia(self):
+
+    self.distancia= math.sqrt(self.puntoA-self.puntoB)
